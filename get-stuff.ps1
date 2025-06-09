047eda4e30f8f2f81724dbb4ae5f4318fb5263e9
Missy Januszko <majst32@yahoo.com>
2025-06-09 19:54:58 -0400

    add comments

--- get-stuff.ps1
+++ get-stuff.ps1
@@ -4,6 +4,7 @@ param()
 
     #get some data
     $data = invoke-restMethod -Method Get -uri "https://jsonplaceholder.typicode.com/albums"
+    #loop around data and classify as Special if conditions are met
     $Result = $Data.foreach({
         $Album = $_
         $Special = test-stuff -album $Album
@@ -28,6 +29,8 @@ param(
     $SpecialWords.foreach({
         if ($Album.Title -like "voluptas $($_)*") {
             $IsSpecial = $true
+            #I want to break out of this foreach loop if it finds the first instance of a special title
+            #but this also causes the loop in get-stuff to terminate and not return any data.
             break
         }
     })
